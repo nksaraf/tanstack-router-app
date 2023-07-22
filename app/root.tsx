d3cdd511923f5f1e248113f048d39074f2bac415
Nikhil Saraf <nsaraf98@gmail.com>
2023-07-23 01:51:46 +0530

    tanstack

---
+++ app/root.tsx
@@ -0,0 +1,56 @@
+import { DehydrateRouter } from "@tanstack/react-start/client";
+import { Link, Outlet, useRouter, useRouterContext } from "@tanstack/router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { createAssets } from "@vinxi/react";
+import { Suspense } from "react";
+
+export default function Root() {
+  const router = useRouter();
+  return (
+    <html>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link rel="icon" href="/favicon.ico" />
+        <Suspense>{router.context.assets}</Suspense>
+        {import.meta.env.DEV ? (
+          <script
+            type="module"
+            src={
+              import.meta.env.MANIFEST["client"].inputs[
+                import.meta.env.MANIFEST["client"].handler
+              ].output.path
+            }
+          />
+        ) : null}
+      </head>
+      <body>
+        <div className="p-2 flex gap-2 text-lg">
+          <Link
+            to="/"
+            activeProps={{
+              className: "font-bold",
+            }}
+            activeOptions={{ exact: true }}
+          >
+            Home
+          </Link>{" "}
+          <Link
+            to={"/posts"}
+            activeProps={{
+              className: "font-bold",
+            }}
+          >
+            Posts
+          </Link>
+        </div>
+        <hr />
+        <Outlet />
+        {/* Start rendering router matches */}
+        <TanStackRouterDevtools position="bottom-right" />
+        <DehydrateRouter />
+      </body>
+    </html>
+  );
+}
